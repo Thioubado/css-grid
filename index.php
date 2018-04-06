72365bebd29ad2213c7624230d871f6d87e20846
Momo <seckndanane16@gmail.com>
2018-04-06 02:21:22 +0100

    modification du div class en nav class

--- index.php
+++ index.php
@@ -13,14 +13,14 @@
 <body>
     <div class="wrapper">
         <!-- NAVIGATION-->
-        <div class="main-nav">
+        <nav class="main-nav">
             <ul>
                 <li><a href="#">home</a></li>
                 <li><a href="#">about</a></li>
                 <li><a href="#">services</a></li>
                 <li><a href="#">contact</a></li>
             </ul>
-        </div>
+        </nav>
         <!-- NAVIGATION ENDS-->
 
     </div>
